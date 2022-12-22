f4a38e02969552f2de88b118f6cee48995d2cd23
Ramazan Daminov <radamin@yandex.ru>
2022-12-22 10:06:52 +0300

    Added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру 
+// этого числа. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstnumber = number / 10;
+int secondnumber = number / 100;
+
+int result = firstnumber - secondnumber*10;
+Console.WriteLine($"Result from {number} -> {result}");
